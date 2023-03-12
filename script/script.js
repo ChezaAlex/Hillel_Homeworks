831d7817bdf163b24909987f4094f982f5663619
AlexnderVAlexander <gorobz30@gmail.com>
2023-03-12 15:57:49 +0200

    first commit

---
+++ script/script.js
@@ -0,0 +1,19 @@
+const DEFAULT_PARAMETRS = [1, 'two', () => console.log('hello'), true, false]; 
+
+function funsinglelist(){
+let SINGLE_LIST_HEAD = {
+    value: 0,
+    next: null
+  }
+  for(let i = DEFAULT_PARAMETRS.length; i; --i){
+    SINGLE_LIST_HEAD.value = DEFAULT_PARAMETRS.pop();
+    SINGLE_LIST_HEAD = {next: SINGLE_LIST_HEAD}
+}
+  let res = SINGLE_LIST_HEAD.next.value;
+  console.log(res)
+}
+
+funsinglelist(DEFAULT_PARAMETRS)
+
+
+
