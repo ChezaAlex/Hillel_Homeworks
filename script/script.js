e3ac8871666453b75eaf462ad7307e574cf7e95e
ChezaAlex <108489824+ChezaAlex@users.noreply.github.com>
2023-04-17 12:05:15 +0300

    Update script.js

--- script/script.js
+++ script/script.js
@@ -1,18 +1,20 @@
 let form = document.querySelector('form')
 let createHeroesWrapper = document.querySelector('#createHeroes')
+let comicsSelect = document.querySelector('#comics');
 
 let render = (hero) =>{
   let heroBlock = document.createElement('form')
   heroBlock.setAttribute('id',`${hero.id}`) 
   heroBlock.className = `${hero.name}`.replace(/ /g,'')
+
   heroBlock.innerHTML = `
     <label for=""> Name:
         <input type="text" data-name="name" value =${hero.name}>
     </label>
     <label for=""> Comics:
-        <select name="" id="" data-name="Comics" value = ${hero.comics}>
-            <option value="DC">DC</option>
-            <option value="Marvel">Marvel</option>
+        <select data-name="Comics">
+            <option value="DC" ${hero.comics == 'DC' ? 'selected' : ''} >DC</option>
+            <option value="Marvel" ${hero.comics == 'Marvel' ? 'selected' : ''}>Marvel</option>
         </select>
     </label>
     <label for="" data-name="Comics"> Favorite
@@ -27,9 +29,15 @@ let render = (hero) =>{
         let idHero = e.target.id
         let deletedNode = document.querySelector(`.${classHero}`)
         let favourite = heroBlock.querySelector('input[class = "getInput"]')
+        let name = heroBlock.querySelector(`input[data-name="name" ]`)
+        let comics = heroBlock.querySelector(`select[data-name="Comics" ]`)
       if (e.submitter.name === "Update") {
-        hero.favourite = favourite.checked
-        changeHero(idHero, hero)
+          let changedHero = {
+                name: name.value,
+                comics: comics.value,
+                favourite: favourite.checked
+          }
+        changeHero(idHero, changedHero)
       }else if(e.submitter.name === "Delete"){
         deletedNode.remove()
         deleteHero(idHero)
@@ -48,8 +56,16 @@ let createHero = async (obj) =>{
   
   render(result)
 }
-
-form.addEventListener('submit', (e) =>{
+let checkIfNameExists = async (name) => {
+  let result = await fetch(`https://63693f7228cd16bba71904e4.mockapi.io/heroes?name=${name}`,{
+    method: 'GET',
+    headers:{
+      "Content-type": "application/json",
+    }
+  }).then(res=>res.json())
+  return result.length > 0;
+}
+form.addEventListener('submit', async  (e) =>{
   e.preventDefault() 
 
   let name = form.querySelector('input[data-name="name"]')
@@ -61,7 +77,12 @@ form.addEventListener('submit', (e) =>{
     comics: comics.value,
     favourite: favourite.value
   }
-  createHero(newHero) 
+  if (await checkIfNameExists(newHero.name)) {
+    console.log(`A hero with name "${newHero.name}" already exists.`);
+    return;
+  }
+  
+  createHero(newHero);
 })
 
 let getExistedHeroes = async() =>{
@@ -75,16 +96,34 @@ let getExistedHeroes = async() =>{
 }
 getExistedHeroes()
 
+let getComicses = async() =>{
+  let result = await fetch(`https://63693f7228cd16bba71904e4.mockapi.io/universes`,{
+  method: 'GET',
+  headers:{
+    "Content-type": "application/json",
+  }
+  }).then(res=>res.json())
+    .then(data => {
+      data.forEach(universe => {
+        let option = document.createElement('option');
+        option.value = universe.name;
+        option.text = universe.name;
+        comicsSelect.add(option);
+      });
+  })
+}
+getComicses()
+
 let deleteHero = async(idHero) =>{
   let result = await fetch(`https://63693f7228cd16bba71904e4.mockapi.io/heroes/${idHero}`,{
     method: 'DELETE',
   }).then(res=>res.json())
 }
 
-let changeHero = async (idHero, hero) =>{
+let changeHero = async (idHero, changedHero) =>{
   let result = await fetch(`https://63693f7228cd16bba71904e4.mockapi.io/heroes/${idHero}`,{
     method: 'PUT',
     headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify(hero)
+    body: JSON.stringify(changedHero)
 }).then(res=>res.json())
 }
