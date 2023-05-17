bce5d16eebd09f57184fc1806d9a1c24400a22d8
ChezaAlex <108489824+ChezaAlex@users.noreply.github.com>
2023-05-17 07:21:04 +0300

    Update login.js

--- script/login.js
+++ script/login.js
@@ -40,60 +40,6 @@ let logJSONData = async (userId) =>{
   localStorage.setItem("user", JSON.stringify(userObj));
 }
 
-//Registration
-
-// const formRegistration = document.getElementById('formRegistration');
-// const errorRegistration = formRegistration.querySelector('.errorReg');
-// errorRegistration.className = `classError`
-//   formRegistration.addEventListener('submit', (e) => {
-//     e.preventDefault();
-
-//     const name = formRegistration.name.value.trim();
-//     const email = formRegistration.email.value.trim();
-//     const password = formRegistration.password.value.trim();
-//     const verifyPassword = formRegistration.verifyPassword.value.trim();
-
-//     if (password !== verifyPassword) {
-//       errorRegistration.innerText = 'Password not matches!';
-//       errorRegistration.style.display = 'block';
-//       return;
-//     }
-
-//     fetch(`https://634e9f834af5fdff3a625f84.mockapi.io/users?email=${email}`)
-//       .then(response => response.json())
-//       .then(users => {
-//         if (users.length > 0) {
-//           errorRegistration.innerText = 'User with email ' + email + ' already exists!';
-//           errorRegistration.style.display = 'block';
-//           return;
-//         }
-
-//         fetch('https://634e9f834af5fdff3a625f84.mockapi.io/users', {
-//           method: 'POST',
-//           headers: { 'Content-Type': 'application/json' },
-//           body: JSON.stringify({
-//             name,
-//             email,
-//             password,
-//             status: true,
-//             orders: [],
-//             shoppingCart: []
-//           })
-//         })
-//         .then(() => {
-//           window.location.href = 'index.html';
-//         })
-//         .catch(() => {
-//           errorRegistration.innerText = 'Error creating user.';
-//           errorRegistration.style.display = 'block';
-//         });
-//       })
-//       .catch(() => {
-//         errorRegistration.innerText = 'Error checking email.';
-//         errorRegistration.style.display = 'block';
-//       });
-//       logJSONData(user.id)
-//   });
 const formRegistration = document.getElementById('formRegistration');
 const errorRegistration = formRegistration.querySelector('.errorReg');
 
