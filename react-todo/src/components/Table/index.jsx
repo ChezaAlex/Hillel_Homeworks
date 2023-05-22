3a48407e7b6e92b691a772bb29e56bfae089e2b3
ChezaAlex <108489824+ChezaAlex@users.noreply.github.com>
2023-05-22 10:48:17 +0300

    Update index.jsx

--- react-todo/src/components/Table/index.jsx
+++ react-todo/src/components/Table/index.jsx
@@ -1,33 +1,33 @@
-import React from 'react'
-import Row from './components/Row'
-import '../../App.css'
+  import React from 'react'
+  import Row from './components/Row'
+  import '../../App.css'
 
-export default function Table({todoList, updateStatus, changeFilter, deleteTodo, updateTitle, }) {
-  return (
-    <>
-        <div className='row'>
-          <button onClick= {(e)=>changeFilter(e)} className='all'>All</button>
-          <button onClick= {(e)=>changeFilter(e)} className='pending'>Pending</button>
-          <button onClick= {(e)=>changeFilter(e)} className='completed'>Completed</button>
-        </div>
-      <thead>
-         <th></th>
-         <th>Name</th>
-         <th>Status</th>
-         <th>Action</th>
-      </thead>
-      <tbody>
-        {todoList.length ? 
-          todoList.map((el, id)=> 
-          <Row 
-            row = {el} 
-            key = {el.id}
-            updateStatus = {updateStatus} 
-            deleteTodo ={deleteTodo}
-            updateTitle={updateTitle}
-            />) 
-            : null}
-      </tbody>
-    </>
-  )
-}
+  export default function Table({todoList, updateStatus, changeFilter, deleteTodo, updateTitle, }) {
+    return (
+      <>
+          <div className='row'>
+            <button onClick= {(e)=>changeFilter(e)} className='all'>All</button>
+            <button onClick= {(e)=>changeFilter(e)} className='pending'>Pending</button>
+            <button onClick= {(e)=>changeFilter(e)} className='completed'>Completed</button>
+          </div>
+        <thead>
+           <th></th>
+           <th>Name</th>
+           <th>Status</th>
+           <th>Action</th>
+        </thead>
+        <tbody>
+          {todoList.length ? 
+            todoList.map((el, id)=> 
+            <Row 
+              row = {el} 
+              key = {el.id}
+              updateStatus = {updateStatus} 
+              deleteTodo ={deleteTodo}
+              updateTitle={updateTitle}
+              />) 
+              : null}
+        </tbody>
+      </>
+    )
+  }
