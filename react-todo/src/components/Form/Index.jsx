21d84cdfb26204c533fba58c0e53598520d48332
ChezaAlex <108489824+ChezaAlex@users.noreply.github.com>
2023-05-22 10:49:17 +0300

    Update Index.jsx

--- react-todo/src/components/Form/Index.jsx
+++ react-todo/src/components/Form/Index.jsx
@@ -1,26 +1,26 @@
-import React, {useState} from 'react'
-import Input from './components/Input'
-import Button from '../Common/Button'
+  import React, {useState} from 'react'
+  import Input from './components/Input'
+  import Button from '../Common/Button'
 
-export default function Form({ createTitle }) {
+  export default function Form({ createTitle }) {
 
 
-  const [title, setTitle] = useState('')
+    const [title, setTitle] = useState('')
 
-  const updateTitle = (value) => {
-    setTitle(value)
+    const updateTitle = (value) => {
+      setTitle(value)
+    }
+
+    const create = () => {
+      setTitle('')
+      createTitle(title)
+    } 
+
+
+    return (
+     <div className="fromWrapper">
+     <Input updateTitle={updateTitle} title={title}/>
+     <Button action={create} text ={"add"}/>
+     </div>
+    )
   }
-
-  const create = () => {
-    setTitle('')
-    createTitle(title)
-  } 
-  
-
-  return (
-   <div className="fromWrapper">
-   <Input updateTitle={updateTitle} title={title}/>
-   <Button action={create} text ={"add"}/>
-   </div>
-  )
-}
