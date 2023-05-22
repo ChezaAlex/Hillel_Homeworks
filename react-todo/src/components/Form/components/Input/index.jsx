820756a795c0f0ac3d2f2ef631115e778d477098
ChezaAlex <108489824+ChezaAlex@users.noreply.github.com>
2023-05-22 10:49:40 +0300

    Update index.jsx

--- react-todo/src/components/Form/components/Input/index.jsx
+++ react-todo/src/components/Form/components/Input/index.jsx
@@ -1,13 +1,13 @@
-import React from 'react'
-import '../../../../App.css'
+  import React from 'react'
+  import '../../../../App.css'
 
-export default function Input({ updateTitle, title }) {
+  export default function Input({ updateTitle, title }) {
 
-  return (
-    <input 
-      type="text" 
-      value={title} 
-      onChange={(e) => updateTitle(e.target.value)}
-      />
-  )
-}
+    return (
+      <input 
+        type="text" 
+        value={title} 
+        onChange={(e) => updateTitle(e.target.value)}
+        />
+    )
+  }
