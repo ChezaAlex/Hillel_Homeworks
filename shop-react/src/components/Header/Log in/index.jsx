559fbd09d9568be29a54a4230c74227c94d4ba47
ChezaAlex <108489824+ChezaAlex@users.noreply.github.com>
2023-05-23 21:16:39 +0300

    Update index.jsx

--- shop-react/src/components/Header/Log in/index.jsx
+++ shop-react/src/components/Header/Log in/index.jsx
@@ -1,21 +1,21 @@
-import React from 'react'
-import { useState, useEffect } from 'react';
-export default function index() {
-  const [user, setUser] = useState(null);
+  import React from 'react'
+  import { useState, useEffect } from 'react';
+  export default function index() {
+    const [user, setUser] = useState(null);
 
-  useEffect(() => {
-    const userFromLocalStorage = localStorage.getItem('user');
-    if (userFromLocalStorage) {
-      setUser(JSON.parse(userFromLocalStorage));
-    }
-  }, []);
-  return (
-    <div>
-      {user ? (
-        <p>Hi, {user.name}!</p>
-      ) : (
-        <a href="#" style={{color: 'white', textDecoration: 'none', paddingRight: '10px'}}>Log in</a>
-      )}
-    </div>
-  )
-}
+    useEffect(() => {
+      const userFromLocalStorage = localStorage.getItem('user');
+      if (userFromLocalStorage) {
+        setUser(JSON.parse(userFromLocalStorage));
+      }
+    }, []);
+    return (
+      <div>
+        {user ? (
+          <p>Hi, {user.name}!</p>
+        ) : (
+          <a href="#" style={{color: 'white', textDecoration: 'none', paddingRight: '10px'}}>Log in</a>
+        )}
+      </div>
+    )
+  }
