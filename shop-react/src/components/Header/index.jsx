b1be13735becd1d10a1db347ebdca85f2a1115b4
ChezaAlex <108489824+ChezaAlex@users.noreply.github.com>
2023-05-23 21:16:24 +0300

    Update index.jsx

--- shop-react/src/components/Header/index.jsx
+++ shop-react/src/components/Header/index.jsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import logo from '../../img/logo.png'
-import shoppingCart from '../../img/shopping-cart.png'
-import LogIn from './Log in/index'
-import './style.sass'
+  import React from 'react'
+  import logo from '../../img/logo.png'
+  import shoppingCart from '../../img/shopping-cart.png'
+  import LogIn from './Log in/index'
+  import './style.sass'
 
 
-export default function index() {
-  return (
-    <div className='header'>
-      <img src={logo} alt="" id='logo' />
-      <div id = 'log'>
-        <LogIn/>
-        <img src={shoppingCart} id='cartImage'/>
+  export default function index() {
+    return (
+      <div className='header'>
+        <img src={logo} alt="" id='logo' />
+        <div id = 'log'>
+          <LogIn/>
+          <img src={shoppingCart} id='cartImage'/>
+        </div>
       </div>
-    </div>
-  )
-}
+    )
+  }
