0503a86b17d761db60226e4e75d4befdd05c17a3
ChezaAkex <gorobz30@gmail.com>
2023-06-09 11:12:36 +0300

    first commit

---
+++ src/components/Header/context.jsx
@@ -0,0 +1,24 @@
+import React, { createContext, useState } from 'react';
+
+
+const CartContext = createContext();
+
+const CartProvider = ({ children }) => {
+  const [cartItems, setCartItems] = useState([]);
+
+  const addToCart = (productId) => {
+    setCartItems((prevItems) => [...prevItems, productId]);
+  };
+
+  const removeFromCart = (productId) => {
+    setCartItems((prevItems) => prevItems.filter((item) => item !== productId));
+  };
+
+  return (
+    <CartContext.Provider value={{ cartItems, addToCart, removeFromCart }}>
+      {children}
+    </CartContext.Provider>
+  );
+};
+
+export { CartContext, CartProvider };
---
+++ src/components/Header/index.jsx
@@ -0,0 +1,69 @@
+import React, { useState, useEffect, useContext } from 'react';
+import { Link } from 'react-router-dom';
+import logo from '../../img/logo.png';
+import shoppingCart from '../../img/shopping-cart.png';
+import './style.sass';
+import { CartContext } from './context';
+
+export default function Header() {
+  const [user, setUser] = useState(null);
+  const [numb, setNumb] = useState(0);
+  const { cartItems } = useContext(CartContext);
+
+  useEffect(() => {
+    const userFromLocalStorage = localStorage.getItem('user');
+    if (userFromLocalStorage) {
+      setUser(JSON.parse(userFromLocalStorage));
+    }
+  }, []);
+
+  useEffect(() => {
+    setNumb(cartItems.length);
+  }, [cartItems]);
+
+  const handleLogOut = () => {
+    localStorage.removeItem('user');
+    window.location.reload(); // Reload the page after clearing localStorage
+  };
+
+  useEffect(() => {
+    const handleStorageChange = (event) => {
+      if (event.key === 'cartItems') {
+        const cartItems = JSON.parse(event.newValue);
+        if (cartItems) {
+          setNumb(cartItems.length);
+        }
+      }
+    };
+
+    window.addEventListener('storage', handleStorageChange);
+
+    return () => {
+      window.removeEventListener('storage', handleStorageChange);
+    };
+  }, []);
+
+  return (
+    <div className='header'>
+      <Link to='/'>
+        <img src={logo} alt='' id='logo' />
+      </Link>
+      <div id='log'>
+        <div className='LogIn'>
+          {user ? (
+            <Link to='/forms' className='logInText'>Hi, {user.name}!</Link>
+          ) : (
+            <Link to='/forms' className='logInText'>Log in</Link>
+          )}
+        </div>
+        <img src={shoppingCart} alt='' id='cartImage' />
+        {numb > 0 && <span className='cartItemCount'>{numb}</span>}
+        {user && (
+          <span onClick={handleLogOut} className='logOut'>
+            Log Out
+          </span>
+        )}
+      </div>
+    </div>
+  );
+}
